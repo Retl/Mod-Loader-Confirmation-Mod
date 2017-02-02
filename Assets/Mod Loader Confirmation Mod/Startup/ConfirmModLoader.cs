53efd9978aca9be73f756f11238daa4bfbdf1e21
Retl <carlis.moore@gmail.com>
2017-02-02 01:18:50 -0500

    Initial Commit | Setup to Build for fp2modkit
    Adds the editor optiosn to build the most common assetbundles to use, as
    well as a button to copy the assembly file to the assets folder for
    inclusion in the assetbundle (you have to manually mark it as part of an
    asset bundle).

---
+++ Assets/Mod Loader Confirmation Mod/Startup/ConfirmModLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfirmModLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string filename = "ModLoaderConfirmed.txt";
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true))
+            {
+            System.DateTime date = System.DateTime.Now;
+            file.WriteLine(date.ToLongDateString() + ", " + date.ToLongTimeString() + " ~ " + filename);
+            }
+        }
+}
